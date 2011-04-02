bbe90c0092d4728edb7eb1d083638623f064b083
Tom Insam <tom@movieos.org>
2011-04-02 15:11:14 +0200

    rearrange the post class - remove the filetype, delegate everything into the slug property, and make posts and pages act much more similarly.

--- post.rb
+++ post.rb
@@ -1,45 +1,49 @@
 require 'yaml'
 
 class Post
-    attr_accessor :base, :filename, :date, :body, :data, :slug, :filetype
+    attr_accessor :base, :filename, :date, :body, :data, :type, :slug
     
     def initialize(base, filename)
         self.base = base
         self.filename = filename
+        
+        self.slug = filename
+        self.date = Date.today
+        self.type = :page
+
+        # the page itself
         self.data = {}
         self.data["layout"] = "post"
+        self.body = ""
 
-        # filenames need to start with a date
-        m = self.filename.match(/^(\d{4})-(\d\d)-(\d\d)-(.+?).(\w+)$/)
+        # page filenames need to start with a date
+        m = self.filename.match(/^_posts\/(\d{4})-(\d\d)-(\d\d)-(.*)$/)
         if m
             self.date = Date.civil(m[1].to_i, m[2].to_i, m[3].to_i)
             self.slug = m[4]
-            self.filetype = m[5]
-        elsif m = self.filename.match(/^(.*)\.([^\.]+)$/)
-            #puts "Can't parse #{ self.filename } as date"
-            self.date = nil
-            self.slug = self.filename
-            self.filetype = nil
-        else
-            puts "Can't parse #{self.filename} at all"
+            self.type = :post
         end
     end
     
     
-    # return bool true iff this file is actually a Jekyll post file
+    # return bool true iff this file is actually a Jekyll source file
     def read
-        if not self.slug
-            # didn't pass init
+        # note to self - the better long-term way of doing this is to read the first
+        # 4 bytes, look fior '---\n', then read till we have the preamble and parse it,
+        # then read the rest, rather than always reading Xk of the file and splitting it
+        # up. But this works.
+        
+        if not File.exists? File.join(self.base, self.filename)
             return false
         end
 
-        # read the first 100k from the file. If the post is longer than this, it'll be truncated.
+        # read the first 500k from the file. If the post is longer than this, it'll be truncated.
         # but we need to limit to a certain point or we'll slurp in the entirety of every file
         # in the folder.
-        content = File.open(File.join(self.base, self.filename), "r") {|f| f.read(100 * 1024) }
+        content = File.open(File.join(self.base, self.filename), "r") {|f| f.read(500 * 1024) }
         
         if not content
-            puts "can't read #{self.filename}"
+            # 0-length file
             return false
         end
         
@@ -56,45 +60,53 @@ class Post
         # text editor.
         self.body = content.split(/---\s*\n/, 3)[2]
         
-        if not self.data["layout"]
-            puts "#{ self.filename } doesn't have a layout - probably not a post"
-            return false
-        end
-
         return true
     end
-    
+
+
     def write
-        # TODO - create _posts folder.
-        # TODO - move this to the store.
-        
-        old_file = File.join(self.base, self.filename)
-        if self.date
-            new_file = File.join(self.base, sprintf("%04d-%02d-%02d-%s.%s", self.date.year, self.date.month, self.date.day, self.slug, self.filetype))
+        # if the format type has changed, the file on disk will have a different filename
+        # (because the format type is the file extension).
+
+        if self.type == :post
+            new_filename = sprintf("_posts/%04d-%02d-%02d-%s", self.date.year, self.date.month, self.date.day, self.slug)
         else
-            new_file = File.join(self.base, self.slug)
+            new_filename = self.slug
         end
         
-        puts "** new_file #{ new_file }"
-        puts "   old_file #{ old_file }"
+        # create surrounding folder.
+        folder = File.dirname File.join(self.base, new_filename)
+        if not File.directory? folder
+            # TODO - fix for recursive mkdir
+            Dir.mkdir folder
+        end
         
-        File.open(new_file + ".temp", "w") do |f|
+        # write a .temp file rather than overwriting the old file, in case something
+        # goes wrong. TODO - Write a hidden file to avoid confusing jekyll
+        tempname = File.join(self.base, new_filename + ".temp")
+        File.open(tempname, "w") do |f|
             f.write YAML.dump( self.data )
-            f.write "---\n"
+            f.write "---\n\n"
             f.write self.body.strip + "\n"
         end
 
-        if new_file != old_file and File.exist? old_file
-            File.delete old_file
+        # if the file extension changed, remove the old file.
+        if new_filename != self.filename and File.exist? File.join(self.base, self.filename)
+            File.delete File.join(self.base, self.filename)
         end
 
-        File.rename( new_file + ".temp", new_file )
+        # replace the file with the temp file we wrote.
+        File.rename( tempname, File.join(self.base, new_filename) )
+
+        self.filename = new_filename
     end
 
     def to_s
-        return "#<Post #{self.title}>"
+        return "#<Post (#{self.type}) #{self.filename}>"
     end
     
+    
+    # some utility accessors to get/set standard values out of the data hash
     def title
         return self.data["title"]
     end
--- store.rb
+++ store.rb
@@ -8,30 +8,14 @@ class Store
     end
   
     def posts
-        post_root = File.join(self.base, "_posts")
-
-        posts = []
-        
-        # not having any posts is a valid thing
-        return posts unless File.directory? post_root
-        
-        Dir.open(post_root).each{|file|
-            if file.match(/^\./)
-                next
-            end
-            if !File.file? File.join(post_root, file)
-                next
-            end
-            post = Post.new(post_root, file)
-            if post and post.read
-                posts << post
-            end
-        }
-        posts = posts.sort_by{|p| p.date }.reverse
-        posts
+        read_all_files.select{|p| p.type == :post }.sort_by{|p| p.date }.reverse
+    end
+    
+    def pages
+        read_all_files.select{|p| p.type == :page }.sort_by{|p| p.filename }
     end
   
-    def pages
+    def read_all_files
         page_root = File.join(self.base)
 
         # recursive folder walk
@@ -40,8 +24,11 @@ class Store
             #puts "walking #{ folder } under #{ base }"
             pages = []
             Dir.open(File.join(base,folder)).each{|file|
-                # drop hidden files and the system jekyll stuff
-                next if file.match(/^[\.\_]/)
+                # drop hidden files
+                next if file.match(/^[\.]/)
+                
+                # don't walk into any magic folders other than _posts
+                next if file.match(/^_/) and file != "_posts"
 
                 full = File.join(base, folder, file)
                 if File.directory? full
@@ -61,35 +48,37 @@ class Store
         return pages
     end
   
-    def getPost(id)
-        post = Post.new(File.join(self.base, "_posts"), id)
+    def get(filename)
+        post = Post.new(self.base, filename)
         if post.read
             return post
         end
         return nil
     end
-
-    def deletePost(id)
-        File.delete(File.join(self.base, "_posts", id))
-    end
-  
-    def getPage(id)
-        post = Post.new(self.base, id)
-        if post.read
-            return post
+    
+    def delete(filename)
+        if File.exists?(File.join(self.base, filename))
+            File.delete(File.join(self.base, filename))
+            return true
         end
-        return nil
+        return false
+    end
+ 
+    def write(post)
+        post.write
     end
 
-    def deletePage(id)
-        File.delete(File.join(self.base, id))
+    def create(type = :page, slug = nil, date = nil)
+        slug ||= (Time.now.to_i * 3).to_s
+        if type == :page
+            return Post.new(self.base, slug)
+        else
+            date ||= Date.today
+            filename = sprintf("%04d-%02d-%02d-%s", date.year, date.month, date.day, slug)
+            return Post.new(File.join(self.base, "_posts"), filename)
+        end
     end
-  
-    def newPost(date, permalink = nil)
-        permalink ||= (Time.now.to_i * 3).to_s
-        filename = sprintf("%04d-%02d-%02d-%s.markdown", date.year, date.month, date.day, permalink)
-        return Post.new(File.join(self.base, "_posts"), filename)
-    end
+    
   
 end
 
